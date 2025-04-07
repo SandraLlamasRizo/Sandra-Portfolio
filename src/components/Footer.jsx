011d760433bce628994274677281a8ed9fbb63db
Sandra <sandrallamasrizo@gmail.com>
2025-04-07 22:01:50 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,14 +5,14 @@ import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 function Footer() {
     
     return  <footer className="footer w-full lg:max-w-7/10 m-auto px-4 py-6 text-center flex flex-col md:flex-row items-center justify-between gap-6">
-            <p className="text-gray-500">portfolio de San</p>
+            <p className="text-gray-500 xl:text-xl">portfolio de San</p>
             <div className="flex items-center justify-center">
                 <CircularText text="SAN*WEB*DEV*"
                     onHover="speedUp"
                     spinDuration={20}
                     className="custom-class" />
             </div>
-            <ul className="flex justify-center gap-4">
+            <ul className="flex justify-center gap-4 xl:text-xl">
                 <li><a className="text-gray-500 hover:text-black"><AwesomeIcon icon={faLinkedinIn} /></a></li>
                 <li><a className="text-gray-500 hover:text-black"><AwesomeIcon icon={faGithub
                 } /></a></li>
